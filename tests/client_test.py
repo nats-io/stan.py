a9e2fd92fd4f60609c716b9333aef163f38bdbcb
Waldemar Quevedo <wally@apcera.com>
2017-09-13 10:35:35 -0700

    Add test for sync publishing

--- tests/client_test.py
+++ tests/client_test.py
@@ -51,3 +51,28 @@ class ClientTest(SingleServerTestCase):
 
         await nc.close()
         self.assertFalse(nc.is_connected)
+
+    @async_test
+    async def test_sync_publish_and_acks(self):
+        nc = NATS()
+        await nc.connect(io_loop=self.loop)
+
+        sc = STAN()
+        await sc.connect("test-cluster", "client-123", nats=nc)
+
+        # Publish a some messages
+        packs = []
+
+        for i in range(0, 10):
+            pack = await sc.publish("hi", b'hello')
+            packs.append(pack)
+            self.assertTrue(len(pack.guid) > 0)
+            self.assertEqual(pack.error, "")
+
+        self.assertEqual(len(packs), 10)
+
+        # Check that we have cleaned up the pub ack map
+        self.assertEqual(len(sc._pub_ack_map), 0)
+
+        await nc.close()
+        self.assertFalse(nc.is_connected)
